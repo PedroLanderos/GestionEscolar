150c3fcd84a7ff32fc02911e2e5b07304231d026
PedroLanderos <developerpjlc@gmail.com>
2025-05-05 11:48:58 -0600

    [feature]: creacion  de dockerfile para subjectsapi y agregacion de la referencia al dockercompose

--- back/Proyecto.SubjectsApi.Solution/SubjectsApi.Presentation/obj/Debug/net8.0/SubjectsApi.Presentation.AssemblyInfo.cs
+++ back/Proyecto.SubjectsApi.Solution/SubjectsApi.Presentation/obj/Debug/net8.0/SubjectsApi.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SubjectsApi.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de80e80e656889f75dedea2a432d44646a262a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b69a7e2f6dbb63979f4e68c74e798898061da4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SubjectsApi.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SubjectsApi.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
