8c2d569c7f0bb5b7e829bd22760ca88aebb84cdf
PedroLanderos <developerpjlc@gmail.com>
2025-05-06 15:55:57 -0600

    [feature]: creacion de ISchedule, ScheduleDto, ScheduleMapper e implementacion de los mismos dentro de la capa application de schedule api

---
+++ back/Proyecto.ScheduleApi.Solution/ScheduleApi.Application/Mapper/ScheduleMapper.cs
@@ -0,0 +1,49 @@
+﻿using ScheduleApi.Application.DTOs;
+using ScheduleApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScheduleApi.Application.Mapper
+{
+    public static class ScheduleMapper
+    {
+        public static ScheduleDTO FromEntity(Schedule entity)
+        {
+            var hora = TimeSpan.TryParse(entity.HoraInicio, out var parsedHora) ? parsedHora : TimeSpan.Zero;
+            var turno = hora < new TimeSpan(12, 0, 0) ? "Matutino" : "Vespertino";
+
+            return new ScheduleDTO(
+                Id: entity.Id,
+                IdUsuario: entity.IdUsuario,
+                IdMateria: entity.IdMateria,
+                HoraInicio: entity.HoraInicio ?? "",
+                Dia: entity.Dia ?? "",
+                Turno: turno
+            );
+        }
+
+        public static Schedule ToEntity(ScheduleDTO dto)
+        {
+            return new Schedule
+            {
+                Id = dto.Id,
+                IdUsuario = dto.IdUsuario,
+                IdMateria = dto.IdMateria,
+                HoraInicio = dto.HoraInicio,
+                Dia = dto.Dia
+                // Turno no se incluye porque no existe en la entidad
+            };
+        }
+
+        public static List<ScheduleDTO> FromEntityList(List<Schedule> entities)
+        {
+            return entities.Select(FromEntity).ToList();
+        }
+
+        public static List<Schedule> ToEntityList(List<ScheduleDTO> dtos)
+        {
+            return dtos.Select(ToEntity).ToList();
+        }
+    }
+}
--- back/Proyecto.ScheduleApi.Solution/ScheduleApi.Application/obj/Debug/net8.0/ScheduleApi.Application.AssemblyInfo.cs
+++ back/Proyecto.ScheduleApi.Solution/ScheduleApi.Application/obj/Debug/net8.0/ScheduleApi.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScheduleApi.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad79eafecc8348a74577cca4d5515ecdf22ecf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f34f25cc72b469b4929336e0b22aeb4f8701a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScheduleApi.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScheduleApi.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
